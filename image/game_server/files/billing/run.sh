71a85e9ac1ab9f221620e6508d8551f7ed950895
流光 <67579722@qq.com>
2018-06-23 12:04:19 +0800

    fix

--- image/game_server/files/billing/run.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Dark.Hades
-# https://github.com/HadesD/TLBB.Billing
-
-# This script will be deprecated soon to
-# using daemonize of linux
-# I consider to use flag: --daemonize -d
-
-./Billing > /dev/null 2>&1 &
-
