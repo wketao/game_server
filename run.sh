507aa62c278f17680f3c0b8d61b06fb5f8cff96b
流光 <67579722@qq.com>
2018-06-23 12:01:10 +0800

    add image branch

--- run.sh
+++
@@ -1,4 +0,0 @@
-docker-compose up -d
-echo " start game Server ......"
-sleep 15
-docker-compose exec game_server run_gs
--- stop.sh
+++
@@ -1,3 +0,0 @@
-echo " stop game Server ......"
-docker-compose exec game_server stop_gs
-docker-compose stop
