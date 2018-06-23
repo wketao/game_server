38a876525a5bbe0789e65a01ef9710005ea88be5
流光 <67579722@qq.com>
2018-06-23 21:54:33 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker-compose up -d
 echo " start game Server ......"
 sleep 15
--- stop.sh
+++ stop.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo " stop game Server ......"
 docker-compose exec game_server stop_gs
 docker-compose stop
